3abdeb0327e69e631c78765789b390a326ac27b0
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-04-14 23:56:10 +0700

    zzdfs

--- export_cdf/NetCDFExporter.cpp
+++ export_cdf/NetCDFExporter.cpp
@@ -9,6 +9,8 @@ using namespace netCDF;
 
 int main(int argc, char **argv)
 {
+
+
     if (argc < 4) {
         std::cout << "Usage: " << argv[0] << " <netcdf_file> <variable> <output_file> [timestep]" << std::endl;
         return 0;
@@ -18,6 +20,7 @@ int main(int argc, char **argv)
     NcDim x = nc.getDim("lon");
     NcDim y = nc.getDim("lat");
     NcDim time = nc.getDim("time");
+
 
     if (x.isNull() || y.isNull()) {
         x = nc.getDim("x");
@@ -34,6 +37,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
+
     double *data = new double[x.getSize() * y.getSize()];
     if (var.getDimCount() == 3) {
         if (argc < 5) {
@@ -49,13 +53,37 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    std::ofstream outf(argv[3]);
-    for (int i = 0; i < y.getSize(); i++)
+    unsigned long numTimesteps = 4500;
+    int px_start = 650;
+    int px_stop = 1200;
+    int py_start = 50;
+    int py_stop = 1200;
+    for (unsigned long px = px_start;px < px_stop;px += 150)
     {
-        for (int j = 0; j < x.getSize(); j++)
-            outf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i * x.getSize() + j] << " ";
-        outf << std::endl;
+        for (unsigned long py = py_start;py < px_stop;py += 250)
+        {
+            std::cout << px << " " << py << std::endl;
+            var.getVar({0, py, px}, {time.getSize(), 1, 1}, data);
+            std::ofstream outmf("mar_" + std::to_string((px)) + "_" + std::to_string(py));
+            for (int i = 0;i < time.getSize();i++)
+            {
+                outmf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << " ";
+                outmf << std::endl;
+            }
+        }
     }
+
+
+
+   // std::ofstream outf(argv[3]);
+   // for (int i = 0; i < y.getSize(); i++)
+  //  {
+        //for (int j = 0; j < x.getSize(); j++)
+        //    outf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i * x.getSize() + j] << " ";
+      //  outf << std::endl;
+    //}
+
+
 
     delete [] data;
 
