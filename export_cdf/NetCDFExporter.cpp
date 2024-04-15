0a4260f96c983c03a33b50929651cbf74935744d
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-04-15 14:43:05 +0700

    update

--- export_cdf/NetCDFExporter.cpp
+++ export_cdf/NetCDFExporter.cpp
@@ -10,6 +10,7 @@ using namespace netCDF;
 int main(int argc, char **argv)
 {
 
+    std::cout<<argc<<std::endl;
 
     if (argc < 4) {
         std::cout << "Usage: " << argv[0] << " <netcdf_file> <variable> <output_file> [timestep]" << std::endl;
@@ -54,33 +55,78 @@ int main(int argc, char **argv)
     }
 
     unsigned long numTimesteps = 4500;
-    int px_start = 650;
-    int px_stop = 1200;
-    int py_start = 50;
-    int py_stop = 1200;
-    for (unsigned long px = px_start;px < px_stop;px += 150)
+    int px_start = std::stoi(argv[5]);
+    int px_stop = std::stoi(argv[6]);
+    int py_start = std::stoi(argv[7]);
+    int py_stop = std::stoi(argv[8]);
+    std::string type = argv[9];
+    std::vector<std::vector<double>> mariograms(35);
+    for (int time_c = 0;time_c < time.getSize();time_c++)
     {
-        for (unsigned long py = py_start;py < px_stop;py += 250)
+        std::cout << time_c << std::endl;
+        var.getVar({(size_t)time_c, (size_t)0, (size_t)0}, {1, (size_t)y.getSize(), (size_t)x.getSize()}, data);
+        int index = 0;
+        for (unsigned long py = py_start;py < py_stop;py += 150)
         {
-            std::cout << px << " " << py << std::endl;
-            var.getVar({0, py, px}, {time.getSize(), 1, 1}, data);
-            std::ofstream outmf("mar_" + std::to_string((px)) + "_" + std::to_string(py));
-            for (int i = 0;i < time.getSize();i++)
+            for (unsigned long px = px_start;px < px_stop;px += 100)
             {
-                outmf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << " ";
-                outmf << std::endl;
+            {
+                mariograms[index++].push_back(data[px + py*x.getSize()]);
             }
         }
+    }
+    }
+
+
+    int index = 0;
+    for (unsigned long py = py_start;py < py_stop;py += 150)
+    {
+
+        for (unsigned long px = px_start;px < px_stop;px += 100)
+        {
+            auto mar =  mariograms[index];
+            std::ofstream outmf(type + "_" + std::to_string(px) + "_" + std::to_string(py));
+            for (int time_c = 0;time_c < time.getSize();time_c++)
+            {
+                outmf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << mar[time_c] << std::endl;
+                if (mar[time_c] != 0)
+                {
+                    std::cout << index << " " << mar[time_c] << std::endl;
+                }
+
+            }
+            index++;
+        }
+
+
     }
 
 
 
-   // std::ofstream outf(argv[3]);
-   // for (int i = 0; i < y.getSize(); i++)
-  //  {
-        //for (int j = 0; j < x.getSize(); j++)
-        //    outf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i * x.getSize() + j] << " ";
-      //  outf << std::endl;
+
+    //    for (unsigned long px = px_start;px < px_stop;px += 100)
+    //    {
+    //        for (unsigned long py = py_start;py < px_stop;py += 150)
+    //        {
+    //            std::cout << px << " " << py << std::endl;
+    //            var.getVar({0, py, px}, {time.getSize(), 1, 1}, data);
+    //            std::ofstream outmf(type +"_" + std::to_string((px)) + "_" + std::to_string(py));
+    //            for (int i = 0;i < time.getSize();i++)
+    //            {
+    //                outmf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << " ";
+    //                outmf << std::endl;
+    //            }
+    //        }
+    //    }
+
+
+
+    // std::ofstream outf(argv[3]);
+    // for (int i = 0; i < y.getSize(); i++)
+    //  {
+    //for (int j = 0; j < x.getSize(); j++)
+    //    outf << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i * x.getSize() + j] << " ";
+    //  outf << std::endl;
     //}
 
 
